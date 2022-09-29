6d83ff48b6da125fae3bbcff86a18fd4ce4cbb9b
dimsssss <caruen@hanmail.net>
2022-09-30 00:34:30 +0900

    feat: ChainEventLog entity 추가

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { SubcriptionModule } from './subscriptions/SubcriptionModule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscriptions } from './subscriptions/entity/Subscriptions';
+import { ChainEventLog } from './subscriptions/entity/ChainEventLog';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: './data/ether.sqlite',
-      entities: [Subscriptions],
+      entities: [Subscriptions, ChainEventLog],
     }),
     SubcriptionModule,
   ],
