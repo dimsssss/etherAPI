1646ac7b2aa33c6a5442b1738284ea31c1fbe328
dimsssss <caruen@hanmail.net>
2022-10-02 00:13:04 +0900

    feat: expose 적용

--- src/subscriptions/dto/CreateSubscriptionsResponse.ts
+++ src/subscriptions/dto/CreateSubscriptionsResponse.ts
@@ -1,10 +1,17 @@
-import { Transform } from 'class-transformer';
+import { Transform, Expose } from 'class-transformer';
 
 export class CreateSubscriptionsResponse {
+  @Expose()
   id: number;
+  @Expose()
   @Transform(({ value }) => [].concat(value.split(',')))
   topics: string[];
+  @Expose()
   contractAddress: string;
+  @Expose()
   createdAt: Date;
+  @Expose()
   updatedAt: Date;
+
+  deletedAt: Date;
 }
